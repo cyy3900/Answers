3b6b0d06bf9b9d11cd4e2927833c1daa39b88bb6
cyy3900 <cyy3900@126.com>
2018-03-24 14:16:14 +0800

    创建一个问答型社交网站

---
+++ src/main/java/com/qfmz/answers/AnswersApplication.java
@@ -0,0 +1,12 @@
+package com.qfmz.answers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnswersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnswersApplication.class, args);
+	}
+}
---
+++ src/test/java/com/qfmz/answers/AnswersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qfmz.answers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnswersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
